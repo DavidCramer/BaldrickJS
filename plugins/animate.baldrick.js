2b2bebbd917da22e6c5718e3fc5611d4802d42f2
Desertsnowman <david@digilab.co.za>
2013-12-12 08:53:31 +0200

    test

--- plugins/animate.baldrick.js
+++ plugins/animate.baldrick.js
@@ -4,14 +4,18 @@
 	$.fn.baldrick.registerhelper('animate', {
 		target	: function (obj){
 			if(obj.params.trigger.data('animate')){
+				var target = obj.params.target.offset(), width = obj.params.target.outerWidth();
+				console.log(target);
 				var copy = obj.params.target.clone().prop('id', 'animator_'+obj.params.target.prop('id')).removeClass(obj.params.loadClass).insertAfter(obj.params.target);
 				obj.params.target.hide();
+				copy.css({position:'absolute', top:target.top, left : target.left, width: width});
 			}
 		},
 		after_target	: function (obj){
 			if(obj.params.trigger.data('animate')){
-				obj.params.target.css({marginTop:-50, opacity:0}).show().animate({opacity: 1, marginTop: 0}, 500);//slideDown(200);
-				$('#animator_'+obj.params.target.prop('id')).css({position: 'absolute', top: 0, width: obj.params.target.outerWidth()}).animate({opacity: 0, top: 50}, 500, function(){
+				var anim = $('#animator_'+obj.params.target.prop('id'));
+				obj.params.target.css({marginTop:-50, opacity:0}).show().animate({opacity: 1, marginTop: 0}, 300);//slideDown(200);
+				anim.animate({opacity: 0, top: anim.offset().top - 50}, 300, function(){
 					//obj.params.target.css({marginTop:-50, opacity:0}).show().animate({opacity: 1, marginTop: 0}, 500);//slideDown(200);
 					$(this).remove();
 				});
